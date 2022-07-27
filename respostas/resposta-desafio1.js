bc6215f2e1db177170274189344b8340479cdea7
Zanardi <gzanardi1212@gmail.com>
2022-07-27 11:02:52 -0300

    first commit

---
+++ respostas/resposta-desafio1.js
@@ -0,0 +1,5 @@
+"use strict";
+let funcionario = {
+    code: 10,
+    name: "John"
+};
---
+++ respostas/resposta-desafio1.ts
@@ -0,0 +1,9 @@
+interface employee {
+    code: number
+    name: string
+}
+
+let funcionario: employee = {
+    code: 10,
+    name: "John"
+}
